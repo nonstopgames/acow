e93a41de2a464e0aee998be8177a79f2ef8cb01e
yyjhao <yyjhao@gmail.com>
2013-01-11 10:36:34 +0800

    A cleaner trace function

--- js/lib/trace.js
+++ js/lib/trace.js
@@ -1,14 +1,10 @@
 /*
  * Copyright (C) 2011-2013 by NonStop Games
  * Please refer to license.txt for licensing details
- * 
+ *
  * trace.js
  *
- * Simple devtime tracing function. Provides
- * environment-dependent debugging output.
- * Named after ActionScript's trace function
- * (although it's a bit inaccurate).
- *
+ * A wrapper for console.log which only executes if window.DEBUG === true
  * Logging is enabled if the global variable DEBUG is set.
  *
  */
@@ -16,42 +12,13 @@
 "use strict";
 
 (function(ns) {
-
-	// Detect client
-	var client = 'unknown';
-	var agent = navigator.userAgent;
-
-	if(agent.indexOf("Safari") != 0-1) client = 'safari';
-	if(agent.indexOf('Chrome')  != -1) client = 'chrome';
-	if(agent.indexOf('Firefox') != -1) client = 'firefox';
-	if(window.opera) client = 'opera';
-
-	// Assign actual tracing function
-	var doTrace = null;
-	switch(client) {
-		case 'safari':
-		case 'chrome':
-		case 'firefox':
-			doTrace = function(arg) {
-				console.log('>> ' + arg);
-			};
-		break;
-		case 'opera':
-			doTrace = function(args) {
-				opera.postError('>> ' + arg);
-			};
-		break;
-		default:
-			// Don't know how to log
-			doTrace = function() {};
-		break;
+	if(ns.console && ns.console.log){
+		ns.trace = function(){
+			if(ns.DEBUG){
+				ns.console.log.apply(ns.console, arguments);
+			}
+		};
+	}else{
+		ns.trace = function(){};
 	}
-
-	// Create front-end
-	ns.trace = function() {
-		if(ns.DEBUG) {
-			doTrace((Array.prototype.splice.call(arguments,0)).join(' '));
-		}
-	};
-	
 })(window);
